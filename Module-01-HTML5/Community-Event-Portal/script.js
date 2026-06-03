7f9b67cf4bbe709f146dbcfe5b41ae6a2f631d85
Gyanvi Singh <gyanvisingh591@gmail.com>
2026-06-03 15:43:43 +0530

    Completed HTML5 Exercises 4 and 5

--- Module-01-HTML5/Community-Event-Portal/script.js
+++ Module-01-HTML5/Community-Event-Portal/script.js
@@ -1,1 +1,12 @@
 console.log("Community Event Portal Loaded");
+
+const form = document.getElementById("eventForm");
+
+form.addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    document.getElementById("confirmation").textContent =
+        "Registration submitted successfully.";
+
+});
