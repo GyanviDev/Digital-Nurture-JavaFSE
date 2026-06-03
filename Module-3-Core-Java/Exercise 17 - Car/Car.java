03a1b9a473398fbdd213f7ff44cf77fad036572a
Gyanvi Singh <gyanvisingh591@gmail.com>
2026-06-03 19:49:21 +0530

    Module 3 Exercises 11-20 completed

---
+++ Module-3-Core-Java/Exercise 17 - Car/Car.java
@@ -0,0 +1,27 @@
+public class Car {
+
+    String make;
+    String model;
+    int year;
+
+    public Car(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public void displayDetails() {
+        System.out.println(
+                year + " " + make + " " + model
+        );
+    }
+
+    public static void main(String[] args) {
+
+        Car car1 = new Car("Toyota", "Camry", 2023);
+        Car car2 = new Car("Honda", "City", 2024);
+
+        car1.displayDetails();
+        car2.displayDetails();
+    }
+}
